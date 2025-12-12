a6203bfab10d6052a8cbbdfb2c88e62536680034
AlpG2308 <alpcan.onur@gmail.com>
2025-12-12 22:48:07 +0100

    added some qwertz maps dont work properly yet

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,22 +1,15 @@
 local map = vim.keymap.set
-
--- Make : easy on German keyboards (ö -> :)
+--map qwertz
 map("n", "ö", ":")
 map("v", "ö", ":")
 map("o", "ö", ":")
-
--- Make [ and ] comfortable (Ctrl-ü / Ctrl-+)
 map("n", "<C-ü>", "[")
 map("n", "<C-+>", "]")
 map("v", "<C-ü>", "[")
 map("v", "<C-+>", "]")
-
--- Make { and } reachable (Ctrl-Shift combos)
 map("n", "<C-S-ü>", "{")
 map("n", "<C-S-+>", "}")
 map("v", "<C-S-ü>", "{")
 map("v", "<C-S-+>", "}")
-
--- Optional: make ` and ~ easier
 map("n", "<C-^>", "`")
 map("n", "<C-°>", "~")
